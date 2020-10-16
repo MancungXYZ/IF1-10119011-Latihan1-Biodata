c2e25fd8bee55bc49cec0f0515d3633e0b029477
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-16 18:04:38 +0700

    IF1-10119011-Latihan1-Biodata

---
+++ src/if1/pkg10119011/latihan1/biodata/IF110119011Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan1.biodata;
+
+/**
+ *
+ * @author Reihan Wiyanda
+ */
+public class IF110119011Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata Mahasiswa");  
+        System.out.println("-------------------");
+        System.out.println("NAMA\t\t: Reihan Wiyanda");
+        System.out.println("ALAMAT\t\t: Jln. Sekeloa 6a");
+        System.out.println("TEMPAT LAHIR\t: Kuningan");
+        System.out.println("TANGGAL LAHIR\t: 19 Juli 2001");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Gaming");
+        System.out.println("MAKES\t\t: Ayam Geprek");
+        System.out.println("MIKES\t\t: Soda");
+    }
+    
+}
